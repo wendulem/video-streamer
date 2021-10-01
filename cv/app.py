38086d5b3f4bebb505d7824a5ce384cb5138e7b8
Connor Johnson <connor.m.johnson@duke.edu>
2021-10-01 04:29:03 -0400

    Removed ML

--- cv/app.py
+++ cv/app.py
@@ -80,16 +80,16 @@ class CVClient(object):
 
 
 def main(camera, use_streamer, server_addr, stream_fps):
-    obj_detect = edgeiq.ObjectDetection(
-            "alwaysai/mobilenet_ssd")
-    obj_detect.load(engine=edgeiq.Engine.DNN)
+    # obj_detect = edgeiq.ObjectDetection(
+    #         "alwaysai/mobilenet_ssd")
+    # obj_detect.load(engine=edgeiq.Engine.DNN)
 
-    print("Loaded model:\n{}\n".format(obj_detect.model_id))
-    print("Engine: {}".format(obj_detect.engine))
-    print("Accelerator: {}\n".format(obj_detect.accelerator))
-    print("Labels:\n{}\n".format(obj_detect.labels))
+    # print("Loaded model:\n{}\n".format(obj_detect.model_id))
+    # print("Engine: {}".format(obj_detect.engine))
+    # print("Accelerator: {}\n".format(obj_detect.accelerator))
+    # print("Labels:\n{}\n".format(obj_detect.labels))
 
-    fps = edgeiq.FPS()
+    # fps = edgeiq.FPS()
 
     try:
         streamer = None
@@ -101,28 +101,30 @@ def main(camera, use_streamer, server_addr, stream_fps):
         with edgeiq.WebcamVideoStream(cam=camera) as video_stream:
             # Allow Webcam to warm up
             time.sleep(2.0)
-            fps.start()
+            # fps.start()
 
             # loop detection
             while True:
                 frame = video_stream.read()
-                results = obj_detect.detect_objects(frame, confidence_level=.5)
-                frame = edgeiq.markup_image(
-                        frame, results.predictions, colors=obj_detect.colors)
+                frame = cv2.rotate(frame, cv2.cv2.ROTATE_90_CLOCKWISE)
+                text="Success"
+                # results = obj_detect.detect_objects(frame, confidence_level=.5)
+                # frame = edgeiq.markup_image(
+                #         frame, results.predictions, colors=obj_detect.colors)
 
                 # Generate text to display on streamer
-                text = ["Model: {}".format(obj_detect.model_id)]
-                text.append(
-                        "Inference time: {:1.3f} s".format(results.duration))
-                text.append("Objects:")
-
-                for prediction in results.predictions:
-                    text.append("{}: {:2.2f}%".format(
-                        prediction.label, prediction.confidence * 100))
+                # text = ["Model: {}".format(obj_detect.model_id)]
+                # text.append(
+                #         "Inference time: {:1.3f} s".format(results.duration))
+                # text.append("Objects:")
+                
+                # for prediction in results.predictions:
+                #     text.append("{}: {:2.2f}%".format(
+                #         prediction.label, prediction.confidence * 100))
 
                 streamer.send_data(frame, text)
 
-                fps.update()
+                # fps.update()
 
                 if streamer.check_exit():
                     break
@@ -130,9 +132,9 @@ def main(camera, use_streamer, server_addr, stream_fps):
     finally:
         if streamer is not None:
             streamer.close()
-        fps.stop()
-        print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
-        print("approx. FPS: {:.2f}".format(fps.compute_fps()))
+        # fps.stop()
+        # print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
+        # print("approx. FPS: {:.2f}".format(fps.compute_fps()))
 
         print("Program Ending")
 
